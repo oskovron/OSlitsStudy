d8f7ca445a164e407da35e891db7d6e09f6e66e5
oscovron <oscovron@gmail.com>
2023-09-28 11:53:48 +0300

    Series Exercism. First edition

---
+++ src/play/exercism/Series.java
@@ -0,0 +1,49 @@
+package play.exercism;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Series {
+
+    private String series;
+    public static void main(String[] args) {
+//        Series  series3 = new Series("123456");
+//        Series  series4 = new Series("123456");
+//        series3.slices(3).forEach(System.out::println);
+//        series4.slices(4).forEach(System.out::println);
+
+//        Series slicesOfTwoOverlap = new Series("9142");
+//        slicesOfTwoOverlap.slices(2).forEach(System.out::println);
+//
+//        Series slicesOfLongSeries = new Series("918493904243");
+//        slicesOfLongSeries.slices(5).forEach(System.out::println);
+
+//        Series series = new Series("12345");
+//        series.slices(6).forEach(System.out::println);
+
+        Series empty = new Series("");
+        empty.slices(1).forEach(System.out::println);
+    }
+
+    public Series(String series) {
+        this.series = series;
+    }
+
+    public List<String> slices(int splitLength) {
+        if (splitLength<1) {
+            throw new IllegalArgumentException("Slice size is too small.");
+        }
+        if (splitLength>series.length()){
+            throw new IllegalArgumentException("Slice size is too big.");
+        }
+        List<String> splittedList = new ArrayList<>();
+        for (int i =0; i<=series.length()+2; i++) {
+            if (series.length()>=splitLength) {
+                String substring = series.substring(0, splitLength);
+                splittedList.add(substring);
+                series = series.substring(1);
+            }
+        }
+        return splittedList;
+    }
+}
